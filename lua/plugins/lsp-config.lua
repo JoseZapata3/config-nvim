3d62156e1a484cd6bd54f597f2bdfab33d99bdcc
Jose Zapata <josezapata75235@gmail.com>
2026-09-08 21:52:20 -0500

    add auto-complation snippets config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,45 +1,49 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {},
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-    },
-    config = function()
-      require("mason-lspconfig").setup {
-          ensure_installed = { "lua_ls", "ts_ls", "pylsp", "postgres_lsp", "elixirls" },
-      }
-    end
-  },
-  {
-  "neovim/nvim-lspconfig",
-  config = function()
-      vim.lsp.config("lua_ls", {
-        settings = {
-            Lua = {
-                  diagnostics = {
-                      globals = { "vim" },
-                  },
-              },
-          },
-      })
-      vim.lsp.enable("lua_ls")
-      vim.lsp.enable("ts_ls")
-      vim.lsp.enable("pylsp")
-      vim.lsp.enable("postgres_lsp")
-      vim.lsp.enable("elixirls")
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "ts_ls", "pylsp", "postgres_lsp", "elixirls" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "hrsh7th/cmp-nvim-lsp" },
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover,{})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action,{})
-    end
-  }
+			vim.lsp.config("*", { capabilities = capabilities }) -- applies to every server
+			vim.lsp.config("lua_ls", {
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
+			vim.lsp.enable("lua_ls")
+			vim.lsp.enable("ts_ls")
+			vim.lsp.enable("pylsp")
+			vim.lsp.enable("postgres_lsp")
+			vim.lsp.enable("elixirls")
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
